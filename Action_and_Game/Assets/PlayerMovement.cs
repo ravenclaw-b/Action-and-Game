2cb528ce59cf943a6bd1271cb5f2afd866eff255
bamdad <bamdad.kho@gmail.com>
2023-09-14 21:17:13 +0300

    added movement and made camera follow player

--- Action_and_Game/Assets/PlayerMovement.cs
+++ Action_and_Game/Assets/PlayerMovement.cs
@@ -9,6 +9,7 @@ public class movement : MonoBehaviour
     float jumpForce = 10f;
     public Rigidbody2D rb;
     public bool isGrounded = false;
+    public float speed = .2f;
     
     void Start()
     {
@@ -21,9 +22,15 @@ public class movement : MonoBehaviour
         if(Input.GetKeyDown(KeyCode.Space) && isGrounded)
         {
             rb.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
+        }
+
+        if(Input.GetAxisRaw("Horizontal") > 0.1f || Input.GetAxisRaw("Horizontal") < -0.1f)
+        {
+            rb.AddForce(new Vector2(Input.GetAxisRaw("Horizontal") * speed, 0f));
         }   
     }
 
+    // Ground check
     void OnCollisionEnter2D(Collision2D other)
     {
         if(other.gameObject.CompareTag("Ground"))
@@ -31,7 +38,7 @@ public class movement : MonoBehaviour
             isGrounded = true;
         }   
     }
-
+    // !Ground check
     void OnCollisionExit2D(Collision2D other) 
     {
         if(other.gameObject.CompareTag("Ground"))
